e25eeb8a90984bde459c429daeebb3890aa3924c
“Adebayo <Adebayo.osunbiyi.a@gmail.com>
2022-05-09 16:32:59 +0100

    kast

--- src/main/java/com/adlogic/BookApi/student/StudentService.java
+++ src/main/java/com/adlogic/BookApi/student/StudentService.java
@@ -2,10 +2,13 @@ package com.adlogic.BookApi.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -21,5 +24,21 @@ public class StudentService {
         return  this.studentRepository.findAll();
     }
 
+    public void addNewStudent(Student student){
+        Optional<Student> studentByEmail = this.studentRepository.findStudentByEmail(student.getEmail());
+        if(studentByEmail.isPresent()){
+            throw  new IllegalStateException("email taken");
+        }
+        this.studentRepository.save(student);
+    }
 
+
+    public void deleteStudent(Long id) {
+        boolean exists = studentRepository.existsById(id);
+        if(!exists){
+            throw  new IllegalStateException("student with id "+ id+" does not exists");
+        }else{
+            this.studentRepository.deleteById(id);
+        }
+    }
 }
